3bfc8a418f1692ecfe8be055a43db9a00886ad61
Charles Srstka <support@charlessoft.com>
2018-08-05 12:06:52 -0500

    Use template strings

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends Component {
     this.setState({
       over: true,
       status: '😸 😸 😸 YOU WIN!!! 😸 😸 😸',
-      sound: window.location + '/snd/purr.mp3'
+      sound: `${window.location}/snd/purr.mp3`
     })
 
     this.updateTopScore(score);
@@ -94,7 +94,7 @@ class App extends Component {
     this.setState({
       over: true,
       status: '😾 😾 😾 MRROW!!! (Scratch, Hiss) 😾 😾 😾',
-      sound: window.location + '/snd/angry_cat.mp3'
+      sound: `${window.location}/snd/angry_cat.mp3`
     });
   }
 
@@ -133,15 +133,15 @@ class App extends Component {
     return (
       <div>
         <audio ref={self => { this.meowSound = self; }}>
-          <source src={ window.location + '/snd/meow.mp3' } type="audio/mpeg">
+          <source src={ `${window.location}/snd/meow.mp3` } type="audio/mpeg">
           </source>
         </audio>
         <audio ref={self => { this.winSound = self; }}>
-          <source src={ window.location + '/snd/purr.mp3' } type="audio/mpeg">
+          <source src={ `${window.location}/snd/purr.mp3` } type="audio/mpeg">
           </source>
         </audio>
         <audio ref={self => { this.loseSound = self; }}>
-          <source src={  + '/snd/angry_cat.mp3' } type="audio/mpeg">
+          <source src={ `${window.location}/snd/angry_cat.mp3` } type="audio/mpeg">
           </source>
         </audio>
       </div>
